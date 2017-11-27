59be0cf4db1f0f5674f005f77b774ef05dbf322f
Elifazio Bernardes da Silva <elifazio@gmail.com>
2017-11-27 21:04:40 -0200

    Layout justado para todas as versoes de tela

--- Calculator/CalculatorManager.swift
+++ Calculator/CalculatorManager.swift
@@ -9,11 +9,14 @@
 import Foundation
 
 struct CalculatorManager {
-    enum Operation {
+    private var accumulator: Double?
+    private var binaryOperationMemory: PreviousBinaryOperation?
+    
+    private enum Operation {
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double, Double) -> Double)
         case equals
-        case unknown
+        case undefined
     }
     
     private struct PreviousBinaryOperation {
@@ -23,63 +26,51 @@ struct CalculatorManager {
         func perform(with secondOperand: Double) -> Double {
             return function(firstOperand, secondOperand)
         }
-        
     }
     
-    private var accumulator: Double = 0.0
-    private var previousOperand: Double = 0.0
-    private var binaryOperationMemory: PreviousBinaryOperation?
-    private let operations: [String: Operation] = [
-        "+": Operation.binaryOperation({ $0 + $1 }),
-        "-": Operation.binaryOperation({ $0 - $1 }),
+    private let operations: Dictionary<String, Operation> = [
+        "√": Operation.unaryOperation(sqrt),
+        "±": Operation.unaryOperation({ -$0 }),
+        "+": Operation.binaryOperation({ (v1, v2) in v1 + v2 }),
+        "–": Operation.binaryOperation({ (v1: Double, v2: Double) -> Double in return v1 - v2 }),
         "⨉": Operation.binaryOperation({ $0 * $1 }),
         "÷": Operation.binaryOperation({ $0 / $1 }),
         "=": Operation.equals,
-        "±": Operation.unaryOperation({ $0 == 0 ? $0 : -$0 }),
-        "%": Operation.binaryOperation({ $0 * ($1 / 100) }),
-        "√": Operation.unaryOperation(sqrt)
+        //"%": Operation.binaryOperation(???)
     ]
     
-    var result: Double {
+    var result: Double? {
         get {
             return accumulator
         }
     }
     
     mutating func performOperation(_ symbol: String) {
-        guard let operation = self.operations[symbol] else { return }
-        
-        switch operation {
-        case .unaryOperation(let op):
-            self.accumulator = op(self.accumulator)
-        case .binaryOperation(let op):
-            if symbol.elementsEqual("%") {
-                self.binaryOperationMemory = PreviousBinaryOperation(function: op, firstOperand: self.binaryOperationMemory?.firstOperand ?? 0)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .unaryOperation(let op):
+                accumulator = (accumulator != nil ? op(accumulator!) : accumulator)
+            case .binaryOperation(let op):
+                if accumulator != nil {
+                    binaryOperationMemory = PreviousBinaryOperation(function: op, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals:
                 doPreviousBinaryOperation()
-            } else {
-                self.binaryOperationMemory = PreviousBinaryOperation(function: op, firstOperand: self.accumulator)
+            default:
+                break
             }
-        case .equals:
-            doPreviousBinaryOperation()
-        default:
-            break
+        }
+    }
+    
+    mutating func doPreviousBinaryOperation() {
+        if binaryOperationMemory != nil && accumulator != nil {
+            accumulator = binaryOperationMemory!.perform(with: accumulator!)
+            binaryOperationMemory = nil
         }
     }
     
     mutating func setOperand(_ operand: Double) {
         accumulator = operand
-        previousOperand = operand
-    }
-    
-    mutating func doPreviousBinaryOperation() {
-        guard let op = self.binaryOperationMemory else { return }
-        self.binaryOperationMemory = PreviousBinaryOperation(function: self.binaryOperationMemory!.function, firstOperand: self.previousOperand)
-        self.accumulator = op.perform(with: self.accumulator)
-    }
-    
-    mutating func clearMemory() {
-        self.accumulator = 0
-        self.previousOperand = 0
-        self.binaryOperationMemory = nil
     }
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,10 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
-    @IBOutlet weak var clearButton: UIButton!
     
-    var userIsTyping: Bool = false
-    var manager = CalculatorManager()
+    private var userIsTyping: Bool = false
+    private var manager = CalculatorManager()
     
     var displayValue: Double {
         get {
@@ -25,35 +24,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchClear(_ sender: UIButton) {
-        if !self.userIsTyping {
-            self.manager.clearMemory()
-        }
-        display.text = "0"
         userIsTyping = false
-        self.clearButton.setTitle("AC", for: .normal)
+        display.text = "0"
     }
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsTyping {
             let textDisplay = display.text ?? ""
-            display.text = textDisplay == "0" && digit == "0" ? textDisplay : textDisplay + digit
+            display.text = textDisplay + digit
         } else {
             display.text = digit
             userIsTyping = true
-            self.clearButton.setTitle("C", for: .normal)
         }
     }
     
     @IBAction func performOperation(_ sender: UIButton) {
         if userIsTyping {
-            self.userIsTyping = false
-            self.manager.setOperand(displayValue)
+            userIsTyping = false
+            manager.setOperand(displayValue)
         }
+
         if let mathSymbol = sender.currentTitle {
-            self.manager.performOperation(mathSymbol)
+            manager.performOperation(mathSymbol)
         }
-        self.displayValue = self.manager.result
         
+        if let result = manager.result {
+            displayValue = result
+        }
     }
 }
